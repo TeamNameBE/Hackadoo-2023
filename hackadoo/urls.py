77e0191600f54c8d15e764877a6562a0cb91374b
Minigrim0 <grimauflorent@gmail.com>
2023-04-01 15:13:40 +0200

    [Upd] Urls

--- hackadoo/urls.py
+++ hackadoo/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from hackadoo import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("api/", include("articles.urls")),
     path('admin/', admin.site.urls),
 ]

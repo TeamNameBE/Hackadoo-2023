e3ed43cfd18592566f3f9b0a286f78b8aa0fa15a
Minigrim0 <grimauflorent@gmail.com>
2023-04-01 14:31:24 +0200

    [Upd] article style

--- frontend/src/components/NewsArticle.vue
+++ frontend/src/components/NewsArticle.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="news-article">
         <h2>{{ title }}</h2>
-        <p>{{ content }}</p>
+        <p class="subhead">{{ content }}</p>
     </div>
 </template>
 
@@ -11,6 +11,34 @@ export default {
     props: {
         title: String,
         content: String
-    }
+    },
 }
 </script>
+
+<style scoped>
+.justified{
+    text-align: justify;
+}
+
+.news-article {
+    transform: scale(1);
+}
+
+.news-article:hover {
+    transform: scale(1.01);
+}
+
+.subhead {
+    initial-letter: 2;
+}
+
+h2 {
+    font-family: "Baskervville", cursive;
+    /* font-size: 1.7em; */
+}
+
+p {
+    font-family: "Baskervville", serif;
+    /* font-size: 1.2em; */
+}
+</style>

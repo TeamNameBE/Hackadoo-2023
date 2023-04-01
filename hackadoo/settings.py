f6fa0cf75f56f3f1218ed89404fe1ce385d8b964
Minigrim0 <grimauflorent@gmail.com>
2023-04-01 02:02:42 +0200

    [Add] Bootstrap + static files

--- hackadoo/settings.py
+++ hackadoo/settings.py
@@ -128,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "hackadoo/assets",
+    BASE_DIR / "frontend/dist",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

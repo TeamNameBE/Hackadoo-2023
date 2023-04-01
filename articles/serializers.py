706582a51932b6893cb7b485c88a3804ff68cdd1
Minigrim0 <grimauflorent@gmail.com>
2023-04-01 23:57:58 +0200

    [Upd] Serializers
    Co-authored-by: yishyu <yishen.yu@chifuri.be>
    Co-authored-by: Nicoflo22 <Nicoflo22@users.noreply.github.com>
    Co-authored-by: Qstevens <Mortinat@users.noreply.github.com>

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 
-from articles.models import Article, Like
+from articles.models import Article, Like, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', )
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('id', 'title', 'abstract', 'url')
+        fields = ('id', 'title', 'abstract', 'url', 'media_url', 'subjects')
+
+    media_url = serializers.SerializerMethodField()
+    subjects = CategorySerializer(read_only=True, many=True)
+
+    def get_media_url(self, obj):
+        if "Sensitive Subjects" in obj.subjects.all().values_list('name', flat=True):
+            return obj.photo_url or None
+        else:
+            return obj.gif_url or None
 
 
 class LikeSerializer(serializers.ModelSerializer):

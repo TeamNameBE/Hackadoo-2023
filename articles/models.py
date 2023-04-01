2ad8dddb4b1d92204d4908eb105b06f624f7afc7
Minigrim0 <grimauflorent@gmail.com>
2023-04-01 16:42:47 +0200

    [Fix] Models

--- articles/models.py
+++ articles/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
 class Article(models.Model):
     url = models.URLField(max_length=200)
     title = models.CharField(max_length=200, default="Insert Title")
-    abstract = models.CharField(max_length=200)
+    abstract = models.TextField(default="Insert content")
     subjects = models.ManyToManyField(Category)
     day = models.IntegerField()
     month = models.IntegerField()

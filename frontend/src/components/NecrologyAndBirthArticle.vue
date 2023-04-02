f1a133b124d3432382fa7d7c1e7274f86a86a6a4
Minigrim0 <grimauflorent@gmail.com>
2023-04-02 13:30:50 +0200

    [Add] Necrology + placeholder

--- frontend/src/components/NecrologyAndBirthArticle.vue
+++ frontend/src/components/NecrologyAndBirthArticle.vue
@@ -1,5 +1,8 @@
 <template>
-    <p>{{ article.title }} - {{ article.year }}</p>
+    <div class="mt-3 col-6">
+        <img :src="get_media_url" class="img-fluid zoomOnHover grayscale aligned" />
+        <h5 class="guy-name">{{ article.title }} - {{ article.year }}</h5>
+    </div>
 </template>
 
 <script>
@@ -8,5 +11,42 @@ export default {
     props: {
         article: Object,
     },
+    computed: {
+        get_media_url() {
+            if(this.article.media_url !== null){
+                return this.article.media_url;
+            } else {
+                return "/static/img/placeholder.jpg";
+            }
+        }
+    },
 }
 </script>
+
+<style>
+.zoomOnHover {
+    transition: transform .3s;
+    transform: scale(1);
+}
+
+.zoomOnHover:hover {
+    transition: transform .3s;
+    transform: scale(1.05);
+}
+
+.grayscale {
+    filter: grayscale(100%);
+}
+
+.aligned {
+    height: 200px;
+    object-fit: cover;
+}
+
+.guy-name {
+    font-family: "BaskervvilleItalic", cursive;
+    font-size: 1.2rem;
+    font-weight: 700;
+    color: #000;
+}
+</style>

d53170d26390f85fb29a68733f2338129727c358
morti <qstevens@hotmail.be>
2023-04-09 20:37:56 +0200

    try to fix nginx

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 export COMPOSE_PROJECT_NAME=gazette
 COMPOSE_FILE=docker-compose.yml
 docker-compose -f docker/$COMPOSE_FILE -p $COMPOSE_PROJECT_NAME up -d --build
+docker-compose -f docker/$COMPOSE_FILE -p $COMPOSE_PROJECT_NAME exec cat /etc/nginx/conf.d
 docker-compose -f docker/$COMPOSE_FILE -p $COMPOSE_PROJECT_NAME restart nginx  # Sometimes the connection between nginx and web fails

87f91570b07c65ae94423688ece1e6303976cea0
morti <qstevens@hotmail.be>
2023-04-09 18:36:06 +0200

    [UPD] wait for db to start up

--- docker/start_django.sh
+++ docker/start_django.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 ./manage.py collectstatic --clear --noinput; ./manage.py migrate; gunicorn hackadoo.wsgi:application --bind 0.0.0.0:8000 --timeout 300
